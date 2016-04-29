a0ca10cf85af71c4dacba044614dc92b97f020e0
Marko <marko.ivanovic93@hotmail.com>
2016-04-29 10:51:29 +0200

    BusMenu

--- app/src/main/java/com/outsourcify/outsourcify/TemplateActivity.java
+++ app/src/main/java/com/outsourcify/outsourcify/TemplateActivity.java
@@ -67,7 +67,7 @@ public class TemplateActivity extends AppCompatActivity {
     }
 
     public void testConfirmation(View view){
-        Intent myIntent = new Intent(TemplateActivity.this, ConfirmationActivity.class);
+        Intent myIntent = new Intent(TemplateActivity.this, BusMenu.class);
         startActivity(myIntent);
     }
 
